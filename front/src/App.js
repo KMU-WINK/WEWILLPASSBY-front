01c801362f5b76aeb6a2881680471d5963a6e8e5
Sanglim00 <sanglim@kookmin.ac.kr>
2021-11-02 21:26:41 +0900

    임시 사이즈 설정

--- front/src/App.js
+++ front/src/App.js
@@ -2,14 +2,22 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import MyPage from "./pages/MyPage";
+import Comment from "./pages/Comment";
 
+const mainStyle = {
+    margin: '0 auto',
+    width: '420px',
+    height: '820px',
+    boxShadow: '0 2px 4px rgb(0 0 0 / 20%)',
+}
 export default function App() {
   return (
-    <div>
+    <div style={mainStyle}>
         <Router>
             <Route exact path="/login" component={Login}/>
             <Route exact path="/home" component={Home}/>
             <Route exact path="/mypage" component={MyPage}/>
+            <Route exact path="/comment" component={Comment}/>
         </Router>
     </div>
   );
