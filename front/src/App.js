eaae27406c561daf5067923d2b33bdb9d672b12c
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2021-11-05 02:46:04 +0900

    Post 컴포넌트 프론트 레이아웃
    스크롤 기능 미구현, 버튼이벤트 미구현

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import MyPage from "./pages/MyPage";
 import Comment from "./pages/Comment";
+import Testing from "./components/common/Post"
 
 const mainStyle = {
     margin: '0 auto',
@@ -18,6 +19,7 @@ export default function App() {
             <Route exact path="/home" component={Home}/>
             <Route exact path="/mypage" component={MyPage}/>
             <Route exact path="/comment" component={Comment}/>
+            <Route exact path="/testing" component={Testing}/>
         </Router>
     </div>
   );
