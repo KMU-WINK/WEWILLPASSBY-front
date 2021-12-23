f6d6d9964c6b15fe22dafd80917a0b53ee5c99a4
Sanglim00 <sanglim@kookmin.ac.kr>
2021-12-23 20:04:41 +0900

    빠른 수정

--- front/src/components/community/PostList.js
+++ front/src/components/community/PostList.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+const myPost = {
+    textOverflow: 'ellipsis',
+    whiteSpace:'nowrap',
+    overflow:'hidden',
+
+}
 const post_title = ({ title, url }) => {
     return(
-        <div onClick={() => {
-            alert("글로 이동합니다.");
-            }}>
+        <div style={myPost}>
             {title}
         </div>
     );
